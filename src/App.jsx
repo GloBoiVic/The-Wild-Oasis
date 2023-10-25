bc76f0d6ad673ddbbef0cdc380ea97e6359170f1
GloBoiVic <victorike08@gmail.com>
2023-10-25 14:44:04 -0500

    Update components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import styled from 'styled-components';
-import GlobalStyles from './styles/GlobalStyles';
-import Button from './ui/Button';
-import Input from './ui/Input';
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard.jsx';
+import Bookings from './pages/Bookings.jsx';
+import Cabins from './pages/Cabins.jsx';
+import NewUsers from './pages/Users.jsx';
+import Settings from './pages/Settings.jsx';
+import Account from './pages/Account.jsx';
+import Login from './pages/Login.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
+import GlobalStyles from './styles/GlobalStyles.js';
+import AppLayout from './ui/AppLayout.jsx';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <H1>Hello World</H1>
-      <Button>Check in</Button>
-      <Input />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<NewUsers />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
