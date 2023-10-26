c6df2309f5c0b4afb9f2d7225a63ff1af3d2551e
GloBoiVic <victorike08@gmail.com>
2023-10-26 13:33:44 -0500

    Add supabase connections

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://jhxoqzmzltyocubaespi.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpoeG9xem16bHR5b2N1YmFlc3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgyNjM5ODAsImV4cCI6MjAxMzgzOTk4MH0.G-pThWKH8FDASVh-RKfJUuvmvvcGRrOCtmGk7USkM-c';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
