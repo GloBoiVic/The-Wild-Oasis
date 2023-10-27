1c5ffb387da1e65c7cb0bb206b615be154052033
GloBoiVic <victorike08@gmail.com>
2023-10-27 14:09:53 -0500

    Add functionality to create new data in supabase

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from './supabase.js';
+import supabase, { supabaseUrl } from './supabase.js';
 
 export async function getCabins() {
   const { data, error } = await supabase.from('cabins').select('*');
@@ -8,6 +8,42 @@ export async function getCabins() {
     throw new Error('Cabins could not be loaded');
   }
 
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  // Adding images to supabase bucket
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    '/',
+    '',
+  );
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // 1. Create cabin
+  const { data, error } = await supabase
+    .from('cabins')
+    .insert([{ ...newCabin, image: imagePath }])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error('Cabins could not be created');
+  }
+
+  // 2. Upload image
+  const { error: storageError } = await supabase.storage
+    .from('cabin-images')
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin IF there was an error uplaoding the image
+  if (storageError) {
+    await supabase.from('cabins').delete().eq('id', data.id);
+    console.error(error);
+    throw new Error(
+      'Cabins image could not be uploaded and the cabin was not created',
+    );
+  }
   return data;
 }
 
