83b46a8a125bfb2b1dd2e86f6c18a914e308ec81
GloBoiVic <victorike08@gmail.com>
2023-10-30 11:30:03 -0500

    Add ability to update settings and cabins

---
+++ src/features/cabins/useUpdateCabin.js
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { createUpdateCabin } from '../../services/apiCabins.js';
+import toast from 'react-hot-toast';
+
+function useUpdateCabin() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateCabin, isLoading: isUpdating } = useMutation({
+    mutationFn: ({ newCabinData, id }) => createUpdateCabin(newCabinData, id),
+    onSuccess: () => {
+      toast.success('Cabin successfully edited');
+      queryClient.invalidateQueries({ queryKey: ['cabins'] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+  return { updateCabin, isUpdating };
+}
+
+export default useUpdateCabin;
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,7 +11,7 @@ export async function getCabins() {
   return data;
 }
 
-export async function createEditCabin(newCabin, id) {
+export async function createUpdateCabin(newCabin, id) {
   const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
 
   // Adding images to supabase bucket
