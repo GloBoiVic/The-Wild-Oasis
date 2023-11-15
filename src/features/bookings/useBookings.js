595743b05eb525c00df63bdffa14e2c2b273e1e4
GloBoiVic <victorike08@gmail.com>
2023-11-15 13:47:40 -0600

    Update custom bookings hook

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -13,7 +13,7 @@ function useBookings() {
     !filterValue || filterValue === 'all' ? null : { field: 'status', value: filterValue };
   // { field: 'totalPrice', value: 5000, method: 'gte' };
 
-  // 2) FILTER
+  // 2) SORT
   // 2a) Get sort value from urlParams
   const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
   // 2b) if sortBy is empty | not defined, return nothing, if different, return a new object with a dynamic value
@@ -22,15 +22,18 @@ function useBookings() {
   const [field, direction] = sortByRaw.split('-');
   const sortBy = { field, direction };
 
+  // 3) PAGINATION
+  const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'));
+
   const {
     isLoading,
-    data: bookings,
+    data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ['bookings', filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ['bookings', filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
-  return { isLoading, error, bookings };
+  return { isLoading, error, bookings, count };
 }
 
 export default useBookings;
