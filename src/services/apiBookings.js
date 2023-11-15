163b5aad292f46c1566e0c177ca9f9cda495c210
GloBoiVic <victorike08@gmail.com>
2023-11-15 13:47:45 -0600

    Update api

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,12 @@
 import { getToday } from '../utils/helpers';
 import supabase from './supabase';
+import { PAGE_SIZE } from '../utils/constants';
 
-export async function getBookings({ filter, sortBy }) {
-  let query = supabase
-    .from('bookings')
-    .select(
-      'id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)',
-    );
+export async function getBookings({ filter, sortBy, page }) {
+  let query = supabase.from('bookings').select(
+    'id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)',
+    { count: 'exact' }, // get the number of results
+  );
 
   // FILTER
   // To filter multiple options, can pass filter as an array and loop through it. For each entry, add a DB query
@@ -15,14 +15,21 @@ export async function getBookings({ filter, sortBy }) {
   // SORT
   if (sortBy) query = query.order(sortBy.field, { ascending: sortBy.direction === 'asc' });
 
-  const { data, error } = await query;
+  // PAGINATION
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
 
   if (error) {
     console.error(error);
     throw new Error('Books could not be loaded');
   }
 
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
