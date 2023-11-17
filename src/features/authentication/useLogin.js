99d52290d654e3e5c7ad3bdaa57d91ec5091dfb6
GloBoiVic <victorike08@gmail.com>
2023-11-17 12:39:00 -0600

    Add login functionalities

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,22 @@
+import { useMutation } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { login as loginApi } from '../../services/apiAuth';
+
+function useLogin() {
+  const navigate = useNavigate();
+
+  const { mutate: login, isLoading: isLoggingIn } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: () => {
+      navigate('/dashboard');
+    },
+    onError: (error) => {
+      console.error(error.message);
+      toast.error(error.message);
+    },
+  });
+  return { login, isLoggingIn };
+}
+
+export default useLogin;
