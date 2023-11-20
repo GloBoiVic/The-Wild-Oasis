763b051467a9661644b3d4bb593f84f6b0f41a29
GloBoiVic <victorike08@gmail.com>
2023-11-19 22:15:58 -0600

    Update components

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from '../../ui/SpinnerMini';
 import useLogin from './useLogin';
 
 function LoginForm() {
-  const [email, setEmail] = useState('vike@test.io');
-  const [password, setPassword] = useState('test1234');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const { login, isLoggingIn } = useLogin();
 
   function handleSubmit(e) {
